34d8299eb7cca191cc03e8a9211f35e66d062667
nFurman <115150157+nFurman@users.noreply.github.com>
2023-01-18 12:33:05 +0200

    first commit

--- u4u_react/src/App.js
+++ u4u_react/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Startpage from "./Startpage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Startpage />
+    </div>
+  );
 }
 
 export default App;
---
+++ u4u_react/src/Startpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Startpage.css";
+
+function Startpage(props) {
+  return (
+    <div className="StartpageContainer">
+      <div className="div1"> </div>
+      <div className="div2"> </div>
+      <div className="div3"> </div>
+    </div>
+  );
+}
+
+export default Startpage;
