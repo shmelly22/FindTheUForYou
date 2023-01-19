4bfe8d21a6b1f0aba87f396f0191cbd55f4bd6d4
Thecoder03 <amdurzachary@gmail.com>
2023-01-19 11:59:00 +0200

    img and search added to body of startpage

--- u4u_react/src/StartBody.js
+++ u4u_react/src/StartBody.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import college1 from "../src/nudes/College1.jpg";
+import college2 from "../src/nudes/College2.jpg";
+import college3 from "../src/nudes/College3.jpg";
 
 function StartBody() {
-  return <div>StartBody</div>;
+  const [college, setCollege] = useState("");
+
+  const handleSearchChange = (event) => {
+    let p = document.getElementById("uTextbox");
+    let text = event.target.value;
+    setCollege(event.target.value);
+    p.textContent = text;
+    console.log(text);
+    if (text.length < 1) {
+      p.textContent = "Search Colleges Here...";
+    }
+  };
+
+  return (
+    <div id="StartBody-Container">
+      <img id="StartB-1" src={college1}></img>
+      <div id="StartB-Search">
+        <div id="searchBox" value="hello">
+          <p id="uTextbox">Search Colleges Here...</p>
+          <input
+            type={"text"}
+            name={"username"}
+            value={college}
+            onChange={(event) => handleSearchChange(event)}
+            required
+            placeholder="Search Colleges Here..."
+            className="CollegeSearch"
+            maxLength={50}
+          ></input>
+        </div>
+      </div>
+      <img id="StartB-2" src={college2}></img>
+      <img id="StartB-3" src={college3}></img>
+    </div>
+  );
 }
 
 export default StartBody;
