403ef9bba69673c56ee9e25e474b77ef8e69f5e9
Thecoder03 <amdurzachary@gmail.com>
2023-01-30 12:22:02 +0200

    Login created and functional, nav bar is dynamic

--- u4u_react/src/Homepage.js
+++ u4u_react/src/Homepage.js
@@ -1,42 +1,10 @@
 import React, { useState, useRef } from "react";
 import StartHeader from "./StartHeader";
-import { dragdropcontext } from "react-beautiful-dnd";
-import { reorderColors } from "./reorder";
-import { ColorMap } from "./types";
-import { AuthorList } from "./AuthorList";
 
 function Homepage() {
-  const [colors, setColors] = useState({
-    a: ["blue", "red"],
-    b: ["pink"],
-    c: ["green"],
-  });
-
   return (
     <>
-      <StartHeader />
-      <DragDropContext
-        onDragEnd={({ destination, source }) => {
-          // // dropped outside the list
-          if (!destination) {
-            return;
-          }
-
-          setColors(reorderColors(colorMap, source, destination));
-        }}
-      >
-        <div>
-          {Object.entries(colors).map(([k, v]) => (
-            <AuthorList
-              internalScroll
-              key={k}
-              listId={k}
-              listType="CARD"
-              colors={v}
-            />
-          ))}
-        </div>
-      </DragDropContext>
+      <div>Homepage</div>
     </>
   );
 }
